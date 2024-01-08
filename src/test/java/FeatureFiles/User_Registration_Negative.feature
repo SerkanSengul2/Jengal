840e84d8a0747911a63c63f42c0baddd892eb3ec
Serkan <serkansengul2@gmail.com>
2024-01-08 23:30:42 +0300

    The task is completed

---
+++ src/test/java/FeatureFiles/User_Registration_Negative.feature
@@ -0,0 +1,10 @@
+Feature: User Registration Negative
+
+  Background:
+    Given  Navigate to site
+    When The user should be able to click register button
+
+  Scenario: User Registration Negative
+    Then The user enters the same data again
+    And The user click on the register button
+    Then The user should be able to see the error message
---
+++ src/test/java/StepDefinitions/User_Registration_Negative.java
@@ -0,0 +1,27 @@
+package StepDefinitions;
+
+import Pages.User_Registration_POM;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+
+public class User_Registration_Negative {
+    User_Registration_POM ur = new User_Registration_POM();
+
+    @Then("The user enters the same data again")
+    public void theUserEntersTheSameDataAgain() {
+        ur.MySendKeys(ur.userName, "TheUseR");
+        ur.MySendKeys(ur.eMail, "usercik2@gmail.com");
+        ur.MySendKeys(ur.password, "TheUseR1234");
+        ur.MySendKeys(ur.passwordConfirmation, "TheUseR1234");
+        ur.MyClick(ur.checkBox);
+    }
+
+    @Then("The user should be able to see the error message")
+    public void theUserShouldBeAbleToSeeTheErrorMessage() {
+        ur.wait.until(ExpectedConditions.visibilityOf(ur.errorMessage));
+        String errorMessage=ur.errorMessage.getText();
+        Assert.assertEquals(errorMessage,"OK");
+        System.out.println("errorMessage = " + errorMessage);
+    }
+}
